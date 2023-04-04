793c9c363e2eba04d71364bbc0327162205e0732
kazimwasti <kazimmwasti@gmail.com>
2023-04-03 23:10:59 -0700

    My first class

---
+++ src/FirstClass.java
@@ -0,0 +1,29 @@
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("SHOPPING LIST");
+        System.out.println("\t- Milk");
+        System.out.println("\t\t> Semi-skimmed");
+        System.out.println("\t\t> Medium");
+        System.out.println("\t- Eggs");
+        System.out.print("\t\t> Free-range\n \t- Bread");
+        caseTwo();
+    }
+
+    public static void caseTwo() {
+        System.out.println("\n\n\nThe");
+        System.out.println("\t Cake");
+        System.out.println("Is\t\t\tA");
+        System.out.print("\tLie.");
+    }
+}
+
+/*
+ SHOPPING LIST
+    - Milk
+            > Semi-skimmed
+            > Medium
+            - Eggs
+            > Free-range
+            - Bread
+ */
